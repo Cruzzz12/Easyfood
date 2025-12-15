a572f1fc3803ece07ec933b8076d30aa4c9c4d91
Cruzzz12 <benja.tag1@gmail.com>
2025-12-14 22:47:17 -0300

    fixeo de render intento

--- recipes_project/settings.py
+++ recipes_project/settings.py
@@ -51,8 +51,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'recipes_project.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=os.environ.get('RENDER', 'False') == 'True', # Opcional: Recomendado para Render
     )
 }
 
